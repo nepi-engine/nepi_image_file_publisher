761509f36e1212a30f9ff19dabe6b18cb7b8a35d
Jason Seawall <jseawall@numurus.com>
2024-10-17 08:08:08 -0700

    initial repo files

---
+++ info/file_pub_img_app_info.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2024 Numurus, LLC <https://www.numurus.com>.
+#
+# This file is part of nepi-engine
+# (see https://github.com/nepi-engine).
+#
+# License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
+#
+
+APP_NAME = 'FILE_PUB_IMG' # Use in display menus
+FILE_TYPE = 'APP'
+APP_DICT = dict(
+    description = 'Application for publishing images from image files',
+    pkg_name = 'nepi_app_file_pub_img',
+    group_name = 'FILE_PUB',
+    config_file = 'app_file_pub_img.yaml',
+    app_file = 'file_pub_img_app_node.py',
+    node_name = 'app_file_pub_img'
+)
+RUI_DICT = dict(
+    rui_menu_name = "Image Publisher", # RUI menu name or "None" if no rui support
+    rui_files = ['NepiAppFilePubImg.js'],
+    rui_main_file = "NepiAppFilePubImg.js",
+    rui_main_class = "FilePubImgApp"
+)
+
+
+
+
